1714a75a0ed19216d70404428ca0f424d9a1944f
i.white <whalterwhite23@gmail.com>
2023-04-25 11:59:17 +0300

    set up the environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>React Admin Dashbooard</h1>
+    <div>
+      <h1 className='underline text-3xl'>React Admin Dashbooard</h1>
     </div>
   );
 }
